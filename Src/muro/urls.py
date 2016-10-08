18748618449ad2bd942c4546e4c9a39303c5ddaa
JulianaRojas <julianarojas03@hotmail.com>
2016-10-08 17:52:13 -0500

    fgfd
    fdgfd

--- Src/muro/urls.py
+++ Src/muro/urls.py
@@ -3,6 +3,7 @@ from .views import *
 
 urlpatterns = [
 	url(r'^lista/$', lista, name="lista"),
+	url(r'^ver/(?P<id>\d+)/$', ver, name="ver"),
 	url(r'^crear/$', crear, name="crear"),
 	url(r'^post/crear$', crear_post, name="crear_post"),
 	url(r'^post/tomar/(?P<id>\d+)/$', tomar, name="tomar"),
